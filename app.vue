c3c453043a3d25e2736c9bf54dc3913c8c613794
Hayk <andreasyanhayk@mail.ru>
2024-05-07 16:11:28 +0400

    design changes

--- app.vue
+++ app.vue
@@ -1,14 +1,33 @@
-<template>
-  <div>
-    <NuxtPage />
+<template style="height: 100%; width: 100%">
+  <div
+    classname="container"
+    style="
+      width: 1300px;
+      height: 700px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    "
+  >
+    <NuxtPage
+      style="display: flex; justify-content: center; align-items: center,height: 100%;"
+    />
   </div>
 </template>
 
-<script setup >
-  import App from '~/app.vue'
-  import { createPinia } from 'pinia';
-  import { createApp } from 'vue'
-  const pinia = createPinia()
-  const app = createApp(App)
-  app.use(pinia)
+<script setup>
+import App from "~/app.vue";
+import { createPinia } from "pinia";
+import { createApp } from "vue";
+const pinia = createPinia();
+const app = createApp(App);
+app.use(pinia);
 </script>
+<style>
+html {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,60 +1,69 @@
-<template>
-  <div>
-    <div>
-      <table>
-        <tr>
-          <td style="width: 70%;">
-            <div id="map" style="width: 100%; min-height: 500px;"></div>
-          </td>
-          <td style="width: 30%; ">
-            <table style="width: 100%;">
-              <tr style="width: 100%;">
-                <td><h4>Սկզբնակետ</h4></td>
-                <td><h4>Վերջնակետ</h4></td>
-              </tr>
-              <tr>
-                <td><h4>{{ graphDataStore.start_name }}</h4></td>
-                <td><h4>{{ graphDataStore.end_name }}</h4></td>
-              </tr>
-            </table>
-            <div style="max-height: 300px; overflow-y: scroll; margin-right: 20px;">
-            <table style="width: 100%; overflow-y: scroll; ">
-              <tr v-for="(data, index) in graphDataStore.rout" :key="index">
-                <td>
-                  <h4>Քայլ {{ index + 1 }}</h4>
-                  <p>Կանգառ N: {{ data.id }} Ավտոբուս N: {{ data.bus }}</p>
-                  <p></p>
-                </td>
-              </tr>
-            </table>
-            </div>
-          </td>
-        </tr>
-      </table>
+<template style="height: 100%">
+  <div classname="tem" style="width: 100%; height: 100%">
+    <div
+      classname="div1"
+      style="
+        width: 100%;
+        min-height: 500px;
+        display: flex;
+        justify-content: center;
+        flex-direction: row;
+        align-items: flex-start;
+        gap: 30px;
+      "
+    >
+      <div id="map" style="width: 100%; height: 100%"></div>
+      <div
+        classname="right-column"
+        style="
+          width: 30%;
+          height: fit-content;
+          display: flex;
+          justify-content: flex-start;
+          flex-direction: column;
+          align-items: center;
+          border: 1px solid black;
+        "
+      >
+        <div
+          classname="div2"
+          style="
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            flex-direction: row;
+            align-items: center;
+            gap: 15px;
+          "
+        >
+          <div>
+            <h4>Սկզբնակետ</h4>
+            <h4>{{ graphDataStore.start_name }}</h4>
+          </div>
+          <div>
+            <h4>Վերջնակետ</h4>
+            <h4>{{ graphDataStore.end_name }}</h4>
+          </div>
+        </div>
+        <div style="max-height: 600px; overflow-y: auto">
+          <div v-for="(data, index) in graphDataStore.rout" :key="index">
+            <h4>Քայլ {{ index + 1 }}</h4>
+            <p>Կանգառ N: {{ data.id }} Ավտոբուս N: {{ data.bus }}</p>
+            <p></p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-
 </template>
 
-<script setup >
-import {useGraphDataStore} from "~/stores/useGraphDataStore.ts";
+<script setup>
+import { useGraphDataStore } from "~/stores/useGraphDataStore.ts";
 const graphDataStore = useGraphDataStore();
 </script>
 
 <style scoped>
-table{
-  padding: 3px;
-  margin: 10px;
-  border-style: solid;
-  border-color: blue;
-  }
-th, td {
-  vertical-align: top; /* Align content at the top of each cell */
-}
-th {
-  text-align: center;
-}
-h4{
+h4 {
   text-align: center;
 }
 </style>
